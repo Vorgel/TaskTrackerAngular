05ea8802d602497dde12777a0843b57551298ff8
Vorgel <lempartkonrad@gmail.com>
2022-06-22 02:29:26 +0200

    Initial Commit

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { UiService } from 'src/app/services/ui.service';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+
+export class AddTaskComponent implements OnInit {
+  @Output() onAddTask:EventEmitter<Task> = new EventEmitter();
+  
+  text: string;
+  day: string;
+  reminder: boolean = true;
+  showAddTask:boolean = false;
+  
+  constructor(private uiService:UiService) { 
+    this.uiService.onToggle().subscribe(value => this.showAddTask = value);
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit():void {
+    if(!this.text){
+      alert('Please add a task');
+      return;
+    }
+
+    const newTask:Task = {
+      text: this.text,
+      day: this.day,
+      reminder: this.reminder
+    }
+
+    this.onAddTask.emit(newTask);
+
+    this.text='';
+    this.day='';
+    this.reminder=false;
+  }
+}
---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TaskService } from 'src/app/services/task.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] = [];
+  
+  constructor(private taskService: TaskService) { }
+
+  ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
+  }
+
+  deleteTask(task:Task): void {
+    this.taskService.deleteTask(task).subscribe(() => this.tasks = this.tasks.filter(t => t.id !== task.id));
+  }
+
+  addTask(task: Task): void {
+    this.taskService.addTask(task).subscribe((task) => this.tasks.push(task));
+  }
+
+  toggleReminder(task:Task):void {
+    task.reminder = !task.reminder;
+    this.taskService.editTask(task).subscribe();
+  }
+}
